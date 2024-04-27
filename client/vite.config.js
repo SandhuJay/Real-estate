0be02bcefc7a53265abde32a6ed6b49bddb9200f
Jay Sandhu <112425757+SandhuJay@users.noreply.github.com>
2024-04-27 16:28:42 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server:{
     proxy:{
       '/api':{
-        target:"http://localhost:5000",
+        target:"https://real-estate-1-6mzi.onrender.com",
       secure:false}
     }
   },
